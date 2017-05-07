8eac87b2e6e6b8721b19e5d8a82c57b074c7c2e0
Ignat Insarov <kindaro@gmail.com>
2017-05-08 02:13:20 +0300

    First working draft.
    % stack exec xgrep -- "p" <<< "<div><p class=div>lalafa</p></div>"
    lalafa%
    % stack exec xgrep -- ".div" <<< "<div><p class=div>lalafa</p></div>"
    lalafa%

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE
+    LambdaCase
+  #-}
+
 module Main where
 
+import XML.Selectors.CSS (toAxis, parsePath)
+import Text.HTML.DOM (parseLT)
+import Text.XML (renderText, def)
+import Text.XML.Cursor (fromDocument, cut, content, child)
+import Control.Arrow ((>>>))
+import Data.Text.Lazy (pack)
+import Data.Text (unpack)
+import System.Environment (getArgs)
+import Data.Maybe (listToMaybe)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    (selector:_) <- getArgs
+    interact (f selector)
+
+    where
+
+    f selector    = pack
+                >>> parseLT
+                >>> fromDocument
+                >>> stuffToError (toAxis . either error id . parsePath $ selector)
+                >>> stuffToError child
+                >>> stuffToError content
+                >>> unpack
+
+    stuffToEither f x = case listToMaybe (f x) of
+                            Nothing -> Left (show x)
+                            Just y -> Right y
+
+    stuffToError :: Show a => (a -> [b]) -> a -> b
+    stuffToError f x = (either error id) . (stuffToEither f) $ x
