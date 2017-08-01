f4ff3e4357ef35f090f0b2ef1c8a277f03d185fb
Ignat Insarov <kindaro@gmail.com>
2017-08-02 01:16:20 +0300

    Add "raw" output option.
    There's no mechanics for options in the design, so I have to pass
    options around. Maybe one day I'll know how to make it better.

--- src/Main.hs
+++ src/Main.hs
@@ -9,27 +9,34 @@ import Text.HTML.DOM (parseLT)
 import Text.XML (renderText, def, Node(NodeElement, NodeContent))
 import Text.XML.Cursor (fromDocument, cut, content, child, node, (&/))
 import Control.Arrow ((>>>))
-import qualified Data.Text.Lazy as Lazy (pack)
+import qualified Data.Text.Lazy as Lazy (pack, toStrict)
 import Data.Text as Strict (pack, unpack)
+import Text.Blaze (toMarkup)
+import Text.Blaze.Renderer.Text (renderMarkup)
 
 import System.Environment (getArgs)
 import Data.Maybe (listToMaybe)
 import Control.Monad
+import Data.List (isPrefixOf)
 
 main :: IO ()
 main = do
 
-    (selector:_) <- getArgs
+    args <- getArgs
+    let options = [ ("raw", "--raw" `elem` args) ]
+    let selector = head . dropWhile (isPrefixOf "--") $ args
     let axis = parsePath >>> either error id >>> toAxis $ selector
-    interact' (f axis)
+    interact' (f options axis)
 
     where
     
     interact' f = interact $ Lazy.pack >>> f >>> fmap unpack >>> unlines
 
-    f axis = parseLT >>> fromDocument >>> (axis &/ g)
+    f options axis = parseLT >>> fromDocument >>> (axis &/ g options)
 
-    g cursor
+    g options cursor
+        | (Just True) <- "raw" `lookup` options =
+            pure . Lazy.toStrict . renderMarkup . toMarkup . node $ cursor
         | (NodeElement x) <- node cursor = pure . (pack . show) $ x
         | (NodeContent x) <- node cursor = pure x
         | otherwise = pure . (pack . show) . node $ cursor
