e294ce5fca83e291003f84087c6d1af2f8205479
Ignat Insarov <kindaro@gmail.com>
2022-09-03 23:51:11 +0200

    Run `fourmolu`.

--- executables/xgrep/Main.hs
+++ executables/xgrep/Main.hs
@@ -1,43 +1,34 @@
-{-# LANGUAGE
-    PatternGuards
-  #-}
-
 module Main where
 
-import Text.XML.Selectors.CSS (toAxis, parsePath)
-import Text.HTML.DOM (parseLT)
-import Text.XML (renderText, def, Node(NodeElement, NodeContent))
-import Text.XML.Cursor (fromDocument, cut, content, child, node, (&/))
 import Control.Arrow ((>>>))
-import qualified Data.Text.Lazy as Lazy (pack, toStrict)
-import Data.Text as Strict (pack, unpack)
-import Text.Blaze (toMarkup)
-import Text.Blaze.Renderer.Text (renderMarkup)
-
-import System.Environment (getArgs)
-import Data.Maybe (listToMaybe)
 import Control.Monad
 import Data.List (isPrefixOf)
+import Data.Maybe (listToMaybe)
+import Data.Text as Strict (pack, unpack)
+import Data.Text.Lazy qualified as Lazy (pack, toStrict)
+import System.Environment (getArgs)
+import Text.Blaze (toMarkup)
+import Text.Blaze.Renderer.Text (renderMarkup)
+import Text.HTML.DOM (parseLT)
+import Text.XML (Node (NodeContent, NodeElement), def, renderText)
+import Text.XML.Cursor (child, content, cut, fromDocument, node, (&/))
+import Text.XML.Selectors.CSS (parsePath, toAxis)
 
-main :: IO ()
+main ∷ IO ()
 main = do
+  args ← getArgs
+  let options = [("raw", "--raw" `elem` args)]
+  let selector = head . dropWhile (isPrefixOf "--") $ args
+  let axis = parsePath >>> either error id >>> toAxis $ selector
+  interact' (f options axis)
+ where
+  interact' f = interact $ Lazy.pack >>> f >>> fmap unpack >>> unlines
 
-    args <- getArgs
-    let options = [ ("raw", "--raw" `elem` args) ]
-    let selector = head . dropWhile (isPrefixOf "--") $ args
-    let axis = parsePath >>> either error id >>> toAxis $ selector
-    interact' (f options axis)
+  f options axis = parseLT >>> fromDocument >>> axis >=> g options
 
-    where
-    
-    interact' f = interact $ Lazy.pack >>> f >>> fmap unpack >>> unlines
-
-    f options axis = parseLT >>> fromDocument >>> (axis >=> g options)
-
-    g options cursor
-        | (Just True) <- "raw" `lookup` options =
-            pure . Lazy.toStrict . renderMarkup . toMarkup . node $ cursor
-        | [NodeContent x] <- node <$> child cursor = pure $ x
-        | (NodeElement x) <- node cursor = pure . (pack . show) $ x
-        | otherwise = pure . (pack . show) . node $ cursor
-
+  g options cursor
+    | (Just True) ← "raw" `lookup` options =
+        pure . Lazy.toStrict . renderMarkup . toMarkup . node $ cursor
+    | [NodeContent x] ← node <$> child cursor = pure x
+    | (NodeElement x) ← node cursor = pure . pack . show $ x
+    | otherwise = pure . (pack . show) . node $ cursor
